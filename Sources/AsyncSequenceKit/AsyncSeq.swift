1cde98eb2c203fdc0fa45819289644e1170f6f9b
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2023-07-01 17:14:57 +0800

    Erasure value type with boxing

---
+++ Sources/AsyncSequenceKit/AsyncSeq.swift
@@ -0,0 +1,102 @@
+//
+//  AsyncSeq.swift
+//
+//
+//  Created by LS Hung on 01/07/2023.
+//
+
+import Foundation
+
+public struct Consumer<Element, Failure: Swift.Error> {
+    let value: Optional<(Element) -> Void>
+    let error: Optional<(Failure) -> Void>
+    let complete: Optional<() -> Void>
+}
+
+public protocol Producer<Element, Failure> {
+    associatedtype Element
+    associatedtype Failure: Swift.Error
+
+    typealias Subscriber = Consumer<Element, Failure>
+    typealias Subscription = _Concurrency.Task<Void, any Swift.Error>
+
+    func receive(subscriber: Subscriber) -> Subscription
+}
+
+extension Producer {
+    public func subscribe(_ subscriber: Subscriber) -> Subscription {
+        return self.receive(subscriber: subscriber)
+    }
+
+    public func subscribe(value: Optional<(Element) -> Void> = nil,
+                          error: Optional<(Failure) -> Void> = nil,
+                          complete: Optional<() -> Void> = nil) -> Subscription {
+        return self.subscribe(Subscriber(value: value, error: error, complete: complete))
+    }
+}
+
+// MARK: - AnyProducer
+
+public struct AnyProducer<Element, Failure: Swift.Error>: Producer {
+    private let box: AnyProducerBox<Element, Failure>
+
+    init(producer: some Producer<Element, Failure>) {
+        if let erased = producer as? AnyProducer<Element, Failure> {
+            self.box = erased.box
+        } else {
+            self.box = AnyProducerBoxImpl(erasure: producer)
+        }
+    }
+
+    public func receive(subscriber: Subscriber) -> Subscription {
+        return box.receive(subscriber: subscriber)
+    }
+}
+
+private class AnyProducerBox<Element, Failure: Swift.Error>: Producer {
+    func receive(subscriber: Subscriber) -> Subscription { fatalError() }
+}
+
+private final class AnyProducerBoxImpl<T: Producer>: AnyProducerBox<T.Element, T.Failure> {
+    private let erasure: T
+    init(erasure: T) { self.erasure = erasure }
+
+    override func receive(subscriber: Subscriber) -> Subscription {
+        return erasure.receive(subscriber: subscriber)
+    }
+}
+
+// MARK: - AsyncSequence
+
+extension AsyncSequence {
+    public func eraseToProducer() -> AnyProducer<Element, Swift.Error> {
+        fatalError()
+    }
+}
+
+
+//public protocol AsyncSeq<Element, Failure> {
+//    associatedtype Element
+//    associatedtype Failure: Swift.Error
+//    associatedtype AsyncSeqItr: AsyncItr<Element, Failure>
+//}
+//public protocol AsyncItr<Element, Failure> {
+//    associatedtype Element
+//    associatedtype Failure: Swift.Error
+//    mutating func next() async -> Result<Element, Failure>?
+//}
+//
+//
+//extension _Concurrency.AsyncStream: AsyncSeq {
+//    public typealias Failure = Never
+//    public func makeItr() -> AsyncSeqItr {
+//        var itr = self.makeAsyncIterator()
+//        return AsyncSeqItr(nextElement: { await itr.next() })
+//    }
+//    public struct AsyncSeqItr: AsyncItr {
+//        let nextElement: () async -> Element?
+//        public mutating func next() async -> Result<Element, Failure>? {
+//            return await self.nextElement().map(Result.success)
+//        }
+//    }
+//}
--- Sources/_Playground/main.swift
+++ Sources/_Playground/main.swift
@@ -44,3 +44,33 @@ for try await e in t { print(e) } // ok
 for await e in s.eraseToAny({ await $0.next() }) { print(e) } // ok
 
 //for try await e in t.eraseToAny({ try await $0.next() }) { print(e) } // bad
+
+
+Task {
+//    do {
+//        for try await e in AsyncStream<Int>.init(Int.self, {
+//            $0.finish()
+//        }) {
+//            print(e)
+//        }
+//    } catch let error as Never {
+//        fatalError()
+//    }
+    await doTry {
+        for try await e in AsyncStream<Int>.init(Int.self, {
+            $0.finish()
+        }) {
+            print(e)
+        }
+    } catch: { error in
+        print(error)
+    }
+}
+
+func doTry(_ action: () async throws -> (), catch onCatch: (Error) -> ()) async {
+    do {
+        try await action()
+    } catch {
+        onCatch(error)
+    }
+}
