4d2a4af29eb72ef649af1bf480408e932a4aedfa
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2023-07-08 14:30:55 +0800

    Add test for subscribing after publishing

--- Tests/_AsyncSequenceKitSubjectTests/PublishSubjectTests.swift
+++ Tests/_AsyncSequenceKitSubjectTests/PublishSubjectTests.swift
@@ -40,4 +40,36 @@ final class PublishSubjectTests: XCTestCase {
         XCTAssertEqual(received.a, [1, 2, 3])
         XCTAssertEqual(received.b, [1, 2, 3])
     }
+
+    func testSubscribingAfterPublishing() async throws {
+        let subject = NoThrowPublishSubject<Int>()
+
+        subject.next(1) // should not be received
+        await Task.megaYield(count: 20)
+
+        let a = Task {
+            var a: [Int] = []
+            for await element in subject {
+                a.append(element)
+            }
+            return a
+        }
+        let b = Task {
+            var b: [Int] = []
+            for await element in subject {
+                b.append(element)
+            }
+            return b
+        }
+        await Task.megaYield(count: 20)
+
+        subject.next(2)
+        subject.next(3)
+        subject.complete()
+        await Task.megaYield(count: 20)
+
+        let received = await (a: a.value, b: b.value)
+        XCTAssertEqual(received.a, [2, 3])
+        XCTAssertEqual(received.b, [2, 3])
+    }
 }
