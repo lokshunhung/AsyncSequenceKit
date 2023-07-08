68bc0bf2deb9be06ac605bace29db0b607b5f126
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2023-07-09 00:53:40 +0800

    Revert "Remove unused files"
    This reverts commit a742aa6cfe6f783af3e988074388cbec128fa70b.

---
+++ Sources/AsyncSequenceKitTypeErasure/TypeErasureDoThrow.swift
@@ -0,0 +1,24 @@
+//
+//  TypeErasureDoThrow.swift
+//
+//
+//  Created by LS Hung on 01/07/2023.
+//
+
+public struct DoThrowAsyncItr<Element>: _Concurrency.AsyncIteratorProtocol {
+    let nextElement: () async throws -> Element?
+
+    public func next() async throws -> Element? {
+        return try await self.nextElement()
+    }
+}
+
+public struct DoThrowAsyncSeq<Element>: _Concurrency.AsyncSequence {
+    public typealias AsyncIterator = DoThrowAsyncItr<Element>
+
+    let asyncIterator: () -> AsyncIterator
+
+    public func makeAsyncIterator() -> AsyncIterator {
+        return self.asyncIterator()
+    }
+}
